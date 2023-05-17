f27554ab159d6ee984bad1660d0ea2382163bc17
Karson Nichols <nicholsk18@gmail.com>
2023-05-16 19:24:36 -0500

    resenter portfolio item

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,7 +3,8 @@ function Portfolio () {
     return (
         <div className="w-11/12 md:container mx-auto">
 
-            <div className="grid grid-cols-1 md:grid-cols-2 grid-flow-row gap-4 -mt-40">
+            {/*<div className="grid grid-cols-3 md:grid-cols-2 grid-flow-row gap-4 -mt-40">*/}
+            <div className="m-auto w-full lg:w-7/12">
                 <div className="bg-light-green-0 rounded-3xl">
                   <a href='https://teamworkconstructionllc.com/' target='_blank'>
                     <img src={teamwork} alt="team work construction project image rounded-3xl" />
