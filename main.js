a2d96068ad2c45ab5b622c0d1fc357a27b245718
Karson Nichols <nicholsk18@gmail.com>
2024-07-11 14:11:19 -0400

    improvements and new projects

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-// document.querySelector("#app").innerHTML = ``;
+const els = Array.from(document.querySelectorAll(".project-section"));
+els.forEach((el) => {
+  addObserver(el);
+});
 
-// setupCounter(document.querySelector('#counter'))
+function addObserver(el) {
+  let observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("animate");
+          // console.log(entry.target.dataset.stop)
+          // callback(entry.target)
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    { threshold: 0.5 }
+  );
+  observer.observe(el);
+}
